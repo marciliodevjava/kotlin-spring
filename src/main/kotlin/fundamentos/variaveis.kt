6d3ad0bdbc960d59287859f475eecc297bf2e033
marciliodevjava <marciliodevjava@gmail.com>
2025-08-25 08:39:43 -0300

    Create classe variaveis

---
+++ src/main/kotlin/fundamentos/variaveis.kt
@@ -0,0 +1,23 @@
+package org.example.fundamentos
+
+/**
+ * Variaveis são algo que vai receber valores
+ * var é do tipo mutavel
+ * val eé do tipo imutavel
+ */
+fun main(){
+    var nome = "Marcilio"
+    val nomeVal = "Daniel"
+    println(nome)
+    println(nomeVal)
+    nome = "Marcilio Zanatta"
+    println(nome)
+}
+
+class variaveis{
+    lateinit var teste: String
+
+    fun iniciaVariveis(){
+        teste = "Teste"
+    }
+}
