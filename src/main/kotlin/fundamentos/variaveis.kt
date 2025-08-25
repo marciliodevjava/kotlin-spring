58ae088fbf07ff1419370fc8876e61585fc79552
marciliodevjava <marciliodevjava@gmail.com>
2025-08-25 09:53:11 -0300

    Update variaveis

--- src/main/kotlin/fundamentos/variaveis.kt
+++ src/main/kotlin/fundamentos/variaveis.kt
@@ -5,19 +5,20 @@ package org.example.fundamentos
  * var é do tipo mutavel
  * val eé do tipo imutavel
  */
-fun main(){
+fun main() {
     var nome = "Marcilio"
     val nomeVal = "Daniel"
+//    nomeVal = "Daniel maestro"
     println(nome)
     println(nomeVal)
     nome = "Marcilio Zanatta"
     println(nome)
 }
 
-class variaveis{
+class variaveis {
     lateinit var teste: String
 
-    fun iniciaVariveis(){
+    fun iniciaVariveis() {
         teste = "Teste"
     }
 }
