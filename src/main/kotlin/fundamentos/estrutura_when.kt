eacbccd99a4991b3bc6353f1e733314bcda8940f
marciliodevjava <marciliodevjava@gmail.com>
2025-08-26 08:27:22 -0300

    Create estrutura_when.kt

---
+++ src/main/kotlin/fundamentos/estrutura_when.kt
@@ -0,0 +1,32 @@
+package org.example.fundamentos
+
+fun main() {
+    println(testawhen(16))
+    testaWhenContador(1, 100, 1)
+    println(comecaComUm("oi tudo bem?"))
+}
+
+fun testawhen(valor: Int): String {
+    return when (valor) {
+        1 -> "Um"
+        2 -> "Dois"
+        3 -> "Três"
+        4 -> "Quatro"
+        5 -> "Cinco"
+        6 -> "Seis"
+        in 11 .. 15 -> "${valor} está entre 11 e 15"
+        !in 16 .. 20 -> "Ele não está entre 16 a 20"
+        else -> "Valor não encontrado"
+    }
+}
+
+fun testaWhenContador(valorIniti: Int, valorFinal: Int, pula: Int) {
+    for (numero in valorIniti..valorFinal step pula) println("Número: ${numero}")
+}
+
+fun comecaComUm(x: Any): Boolean{
+    return when(x){
+        is String -> x.startsWith("oi")
+        else -> false
+    }
+}
